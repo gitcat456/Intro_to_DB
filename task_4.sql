3267d163352d73fd2dd22c3b0eb3a3e8652d901e
gitcat456 <okothdenzel65@gmail.com>
2025-10-20 08:06:10 +0300

    minor bug fixes

--- task_4.sql
+++ task_4.sql
@@ -8,7 +8,7 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = 'alx_b0ok_store'
+    TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'Books'
 ORDER BY 
     ORDINAL_POSITION;
